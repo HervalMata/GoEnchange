04fb69dc950bc9ed661674284d13afd8a1384471
Herval Mata <hervalmataamparo@gmail.com>
2022-07-17 15:25:59 -0300

    feat:art: GoEnchange - created repository appointment

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,26 +1,26 @@
 import { Router } from "express";
-import { startOfHour, parseISO, isEqual } from "date-fns";
-import Appointment from "../entities/Appointment";
+import { startOfHour, parseISO } from "date-fns";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository;
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
   if (findAppointmentInSameDate) {
     return res.status(400).json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
+
 })
 
 export default appointmentsRouter;
