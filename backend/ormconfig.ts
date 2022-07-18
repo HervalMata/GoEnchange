d04c07be426138f3908b4b8e08b20b80e67079c8
Herval Mata <hervalmataamparo@gmail.com>
2022-07-18 19:14:57 -0300

    feat:art: goexchange - create migration appointment

---
+++ backend/ormconfig.ts
@@ -0,0 +1,17 @@
+import {DataSource} from "typeorm";
+
+export const connectionSource = new DataSource({
+  migrationsTableName: "migrations",
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "math3036",
+  database: "goexchange",
+  migrations: [
+    "./src/database/migrations/*.ts"
+  ],
+  /*"cli": {
+    "migrationsDir": "./src/database/migrations"
+  }*/
+});
