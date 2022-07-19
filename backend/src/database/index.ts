451c07e71d4bca70c4efa58114459e403834604e
Herval Mata <hervalmataamparo@gmail.com>
2022-07-19 13:30:00 -0300

    feat:art: goexchange - created model and migration for user

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,3 +1,8 @@
-import {createConnection} from 'typeorm';
+import { DataSource } from 'typeorm';
+// @ts-ignore
+import ormconfig from '../../ormconfig.json';
 
-createConnection();
+export const dataSource = new DataSource({ ...ormconfig});
+dataSource.initialize()
+  .then(() => console.log('Data Source has been initialized successfully!!!'))
+  .catch((err) => console.log('Error during Data Source initialization!!!', err));
--- backend/src/database/migrations/1658247023943-CreateUsers.ts
+++ backend/src/database/migrations/1658247023943-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm"
 
-export class CreateAppointments1658179881227 implements MigrationInterface {
+export class CreateUsers1658247023943 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.createTable(
         new Table({
-          name: "appointments",
+          name: "users",
           columns: [
             {
               name: "id",
@@ -14,13 +14,19 @@ export class CreateAppointments1658179881227 implements MigrationInterface {
               generationStrategy: "uuid"
             },
             {
-              name: "provider",
+              name: "name",
               type: "varchar",
               isNullable: false
             },
             {
-              name: "date",
-              type: "timestamp with time zone",
+              name: "email",
+              type: "varchar",
+              isNullable: false,
+              isUnique: true
+            },
+            {
+              name: "password",
+              type: "varchar",
               isNullable: false
             },
             {
@@ -39,7 +45,7 @@ export class CreateAppointments1658179881227 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable("appointments");
+      await queryRunner.dropTable("users");
     }
 
 }
