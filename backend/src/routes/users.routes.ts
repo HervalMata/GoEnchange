5cb740b378ff0653320b07fd5d1bc2c1231bf320
Herval Mata <hervalmataamparo@gmail.com>
2022-07-19 22:04:05 -0300

    feat:art: goexchange - created avatar route in users

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import {Router} from "express";
 import CreateUserService from "../services/CreateUserService";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import multer from "multer";
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (req, res) => {
   try {
@@ -19,6 +23,10 @@ usersRouter.post("/", async (req, res) => {
     // @ts-ignore
     return res.status(400).json({ error: err.message });
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (req, res) => {
+  return res.json({ ok: true });
 });
 
 export default usersRouter;
