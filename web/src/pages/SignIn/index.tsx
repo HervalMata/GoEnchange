a3282a5dffe414604ea5336c089444cd5c4cd08c
Herval Mata <hervalmataamparo@gmail.com>
2022-07-20 17:33:58 -0300

    feat:art: goexchange - created button and input components

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
 import logoImg from '../../assets/logo.png';
 import { Container, Content, Background } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -12,10 +14,10 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu login</h1>
 
-        <input placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
